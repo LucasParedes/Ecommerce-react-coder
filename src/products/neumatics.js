25528ed5ba4e93c72474c90417cafe6f94a8a7c6
Lucas Paredes <lutesting2@gmail.com>
2024-06-14 00:20:45 -0300

    14-06: Se creo el array de productos para prueba

---
+++ src/products/neumatics.js
@@ -0,0 +1,106 @@
+export const neumatics = [
+  {
+    id: 1,
+    marca: "FATE",
+    modelo: "Prestiva",
+    tamaño: "165/70R13",
+    precio: 83571,
+    stock: 15,
+    descripcion: {
+      info: "Un neumatico equilibrado que brinda la máxima seguridad y confiabilidad,atendiendo los requerimientos de durabilidad y confort de marcha en un segmento dominado por usuarios particulares.Una actualización de la paleta de productos para un mercado en constante evolución",
+      velocidad: "hasta 190 km/h",
+    },
+    categoria: ["cubiertas", "auto"],
+    imagen:
+      "https://acdn.mitiendanube.com/stores/001/173/145/products/prestiva_21-kilgelmann-fate-copia1-723e21ba94197e60d716270463570811-640-0.png",
+  },
+  {
+    id: 2,
+    marca: "FATE",
+    modelo: "Eximia Pininfarina",
+    tamaño: "195/50R16",
+    precio: 176489,
+    stock: 10,
+    descripcion: {
+      info: "Diseñado para responder a las más altas exigencias de los automóviles, tanto en los aspectos de seguridad y confort como en la manejabilidad. Es un neumático equilibrado y seguro, transmitiéndolo al conductor en todas las circunstancias de manejo. Eximia by Pininfarina Green cuenta con la nueva tecnología “Green System” enmarcándose en los llamados neumáticos verdes o ecológicos. Cuya característica distintiva es el bajo valor de resistencia a la rodadura.",
+      velocidad: "Rango V: hasta 240 Km/h",
+    },
+    categoria: ["cubiertas", "auto"],
+    imagen:
+      "https://acdn.mitiendanube.com/stores/001/173/145/products/pininfaria-eco_2-fate-kilgelmann91-db2ce8a069e9c531b016269589691211-640-0.png",
+  },
+  {
+    id: 3,
+    marca: "BF GOODRICH",
+    modelo: "Radial T/A",
+    tamaño: "235/60R15",
+    precio: 240800,
+    stock: 20,
+    descripcion: {
+      info: "BFGoodrich® cuenta con casi 40 años de tradición y experiencia en carreras. BFGoodrich Radial T/A es el primer neumático de altas prestaciones en Norteamérica. Todo un clásico, con más de 50 años de presencia en el mercado.BFGoodrich® Radial T/A es un neumático diseñado para equipar coches deportivos clásicos, matriculados por primera vez antes del 1 de octubre de 1990.BFGoodrich® Radial T/A no se puede montar en coches matriculados por primera vez después del 1 de octubre de 1990",
+      velocidad: "hasta 180 km/h",
+    },
+    categoria: ["cubiertas", "auto"],
+    imagen:
+      "https://aecbmesvcm.cloudimg.io/v7/https://cxf-prod.azureedge.net/b2c-experience-bfg-production/attachments/cl7fu8l9604e301ntbw45hu2d-4w-490-3528708261297-tire-bfgoodrich-trail-terrain-t-slash-a-265-slash-70-r17-115t-nl-a-main-1-30.png?w=412&h=412&org_if_sml=1&func=boundmin",
+  },
+  {
+    id: 4,
+    marca: "MICHELIN",
+    modelo: "Pilot Alpin 5",
+    tamaño: "225/65R17",
+    precio: 489495,
+    stock: 8,
+    descripcion: {
+      info: "control y precisión de conducción:Mejor adherencia en mojado y agarre en nieve. Seguridad en condiciones invernales severas. En invierno mejora la frenada en cualquier superficie (seca, mojada o con nieve). Alta resistencia al aquaplaningReduce el consumo de carburante y mejora la duración. Optimiza la resistencia a la rodadura. Mayor duración en comparación con sus principales competidores. Homologado por Audi, AMG, BMW, Mercedes y Porsche.",
+      velocidad: "hasta 210 km/h",
+    },
+    categoria: ["cubiertas", "camioneta", "suv"],
+    imagen:
+      "https://galicia.com.uy/wp-content/uploads/2020/09/Continental-PremiumContact-6.png",
+  },
+  {
+    id: 5,
+    marca: "CONTINENTAL",
+    modelo: "Premium Contact 2",
+    tamaño: "205/70R16",
+    precio: 312458,
+    stock: 5,
+    descripcion: {
+      info: "Segmentos con borde de perfil desvanecido.Pisada firme que expulsa rápidamente el agua.Ranuras de desagüe 3-D.Flujo de agua como si fuera un parabrisas para evitar el acuaplaneo.Diseño de banda de rodaje asimétrica",
+      velocidad: "hasta 210 km/h",
+    },
+    categoria: ["cubiertas", "camioneta", "suv"],
+    imagen:
+      "https://galicia.com.uy/wp-content/uploads/2020/09/Continental-PremiumContact-2.png",
+  },
+  {
+    id: 6,
+    marca: "CONTINENTAL",
+    modelo: "ContiRoadAttack 2 EVO",
+    tamaño: "190/55ZR17",
+    precio: 210454,
+    stock: 17,
+    descripcion: {
+      info: "Innovador compuesto para altos rendimientos kilométricos manteniendo un extraordinario comportamiento sobre seco y mojado.Gran control y manejabilidad.La especial construcción de la carcasa permite ser equipado en las motos más exigentes.Diseño del perfil exclusivo, que proporciona un aspecto excelente.Gran Oferta de dimensiones: desde scooters de 50 cc. hasta scooters de alta cilindrada.",
+      velocidad: "hasta 150 km/h",
+    },
+    categoria: ["cubiertas", "moto"],
+    imagen: "https://www.mcas.com.au/assets/full/240105.png?20231010151302",
+  },
+  {
+    id: 7,
+    marca: "CONTINENTAL",
+    modelo: "ContiAttack SM",
+    tamaño: "110/70R17",
+    precio: 177450,
+    stock: 9,
+    descripcion: {
+      info: "xtraordinaria manejabilidad y agarre en curva.Excepcional rendimiento y respuesta al límite..Construcción con cinturón de acero 0º para mayor estabilidad y respuesta, tanto en la aceleración como en la frenada en curva..Neumático de rápido calentamiento y buen agarre en frío, gracias al compuesto..La tecnología MultiGrip asegura una larga duración y el máximo grip en los mayores ángulos de inclinación..TractionSkin: una revolucionaria superficie del neumático que evita que „patine“ recién montado.",
+      velocidad: "hasta 210 km/h",
+    },
+    categoria: ["cubiertas", "moto", "scooter"],
+    imagen:
+      "https://cycletreads.co.nz/cdn/shop/products/f9b81ed25140241ecd284a5ff2a0a155_grande.png?v=1677808322",
+  },
+];
