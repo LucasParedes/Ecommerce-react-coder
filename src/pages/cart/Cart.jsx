5412c27b4629da6ec6804410cf7ced9ac8710518
Lucas Paredes <lutesting2@gmail.com>
2024-06-30 18:47:09 -0300

    ADD-30-06: Se creo la pagina del carrito

---
+++ src/pages/cart/Cart.jsx
@@ -0,0 +1,16 @@
+export const Cart = () => {
+  return (
+    <h1
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "15px",
+        justifyContent: "center",
+        padding: "13px",
+        marginTop: "110px", // Ajusta este valor según la altura de tu NavBar
+      }}
+    >
+      CART
+    </h1>
+  );
+};
