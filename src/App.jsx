66084b94c5fcf44e20c22e748ea9deb7c69a4f32
Lucas Paredes <lutesting2@gmail.com>
2024-06-30 18:48:14 -0300

    ADD-30-06: Se agrego react router dom al proyecto, para el tema de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import "./App.css";
 import { NavBar } from "./components/navbar/NavBar";
-import { ItemListContainer } from "./pages/itemListContainer/ItemListContainer";
 import { Footer } from "./components/footer/Footer";
+import { Cart } from "./pages/cart/Cart";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ItemListContainer } from "./pages/itemListContainer/ItemListContainer";
+import { ItemDetailContainer } from "./pages/itemDetailContainer/ItemDetailContainer";
 
 function App() {
   return (
-    <div className="app">
+    <BrowserRouter>
       <NavBar />
-      <div className="content">
-        <ItemListContainer />
-      </div>
-      <Footer />
-    </div>
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/categoria/:categoria" element={<ItemListContainer />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
+      </Routes>
+      {/*       <Footer /> */}
+    </BrowserRouter>
   );
 }
 
