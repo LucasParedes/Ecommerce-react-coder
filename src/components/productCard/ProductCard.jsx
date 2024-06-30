244f7d7d1635f7ba6977d368c77ef177674f5913
Lucas Paredes <lutesting2@gmail.com>
2024-06-30 18:45:43 -0300

    ADD-30-06: Se modificaron los componentes por la libreria de material ui

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,16 +1,47 @@
 import "./productCard.css";
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-export const ProductCard = ({ marca, modelo, precio, img }) => {
+export const ProductCard = ({ marca, modelo, img, id }) => {
   return (
-    <div className="cardContainer">
-      <h1 className="brand">{marca}</h1>
-      <h2 className="model">{modelo}</h2>
-      <img className="imgCard" src={img} alt={`${marca} ${modelo}`} />
-      <h3 className="price">${precio}</h3>
-      <div>
-        <button autofocus> Comprar </button>
-        <button> Consultar Mas </button>
-      </div>
-    </div>
+    <Card
+      sx={{
+        width: 300,
+        backgroundColor: "#832a7a0f",
+        borderRadius: "20px",
+        border: "2px solid #cfa4a4",
+      }}
+    >
+      <CardMedia component="img" alt={marca} height="300" image={img} />
+      <CardContent
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography gutterBottom variant="h5" component="div">
+          {marca}
+        </Typography>
+        <Typography variant="body2">{modelo}</Typography>
+      </CardContent>
+      <CardActions sx={{ justifyContent: "center" }}>
+        <Link to={`/itemDetail/${id}`}>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{ backgroundColor: "#1b3039", color: "white" }}
+          >
+            Ver Mas
+          </Button>
+        </Link>
+      </CardActions>
+    </Card>
   );
 };
