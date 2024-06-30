6e4d4a5d4c1f51eb94cba62a04799d04890d14bc
Lucas Paredes <lutesting2@gmail.com>
2024-06-30 18:46:52 -0300

    FIX-30-06: Se arreglo el navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,32 +1,31 @@
-import { CartWidget } from "../cartWidget/CartWidget";
 import "./navBar.css";
+import { CartWidget } from "../cartWidget/CartWidget";
+import { Link, Outlet } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <header className="header">
-      <div className="logoCompany">
-        <img src="/logo.png" alt="Logo Company" />
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src="/logo.png" alt="Company Logo" className="logoCompany" />
+        </Link>
       </div>
-      <nav className="nav-container">
-        <ul className="nav-links">
-          <li>
-            <a href="#">Productos</a>
-          </li>
-          <li>
-            <a href="#">Servicios</a>
-          </li>
-          <li>
-            <a href="#">Sucursales</a>
-          </li>
-          <li>
-            <a href="#">Nosotros</a>
-          </li>
-          <li>
-            <a href="#">Contacto</a>
-          </li>
-        </ul>
-      </nav>
-      <CartWidget />
-    </header>
+      <div className="navbar-links">
+        <Link to="/">Todas</Link>
+        <Link to="/categoria/auto">Autos</Link>
+        <Link to="/categoria/camioneta">Camionetas</Link>
+        <Link to="/categoria/moto">Motos</Link>
+        <Link to="/contacto">Contacto</Link>
+      </div>
+      <div className="navbar-cart">
+        <CartWidget />
+      </div>
+      <div className="navbar-login">
+        <Link to="/login">Login</Link>
+      </div>
+      <>
+        <Outlet />
+      </>
+    </nav>
   );
 };
