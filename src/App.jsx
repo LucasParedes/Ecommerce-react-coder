853ddb7445007edd9655a4b45551b4ca442653c3
Lucas Paredes <lutesting2@gmail.com>
2024-07-21 23:19:25 -0300

    Se agrego la libreria de toaster y estilos nuevos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import "./App.css";
 import { NavBar } from "./components/navbar/NavBar";
-import { Footer } from "./components/footer/Footer";
 import { Cart } from "./pages/cart/Cart";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ItemListContainer } from "./pages/itemListContainer/ItemListContainer";
 import { ItemDetailContainer } from "./pages/itemDetailContainer/ItemDetailContainer";
+import { Checkout } from "./pages/checkout/Checkout";
+import { CartContext } from "./context/CartContext";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<ItemListContainer />} />
-        <Route path="/categoria/:categoria" element={<ItemListContainer />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
-      </Routes>
-      {/*       <Footer /> */}
+      <Toaster richColors position="top-right" />
+      <CartContext>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/categoria/:categoria" element={<ItemListContainer />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
+          <Route path="/cart/checkout" element={<Checkout />} />
+          <Route
+            path="*"
+            element={<h1 className="not-found"> 404 Not found</h1>}
+          />
+        </Routes>
+      </CartContext>
     </BrowserRouter>
   );
 }
